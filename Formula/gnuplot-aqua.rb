e7b00af65f9ce47f81ac30650418f758cd43e7af
Michael Kenney <mfkenney@gmail.com>
2019-07-25 22:19:04 -0700

    Initial check-in.

---
+++ Formula/gnuplot-aqua.rb
@@ -0,0 +1,51 @@
+class Gnuplot < Formula
+  desc "Command-driven, interactive function plotting"
+  homepage "http://www.gnuplot.info/"
+  url "https://downloads.sourceforge.net/project/gnuplot/gnuplot/5.2.7/gnuplot-5.2.7.tar.gz"
+  sha256 "97fe503ff3b2e356fe2ae32203fc7fd2cf9cef1f46b60fe46dc501a228b9f4ed"
+  revision 1
+
+  head do
+    url "https://git.code.sf.net/p/gnuplot/gnuplot-main.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "gd"
+  depends_on "libcerf"
+  depends_on "lua"
+  depends_on "pango"
+  depends_on "readline"
+
+  def install
+    args = %W[
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --prefix=#{prefix}
+      --with-readline=#{Formula["readline"].opt_prefix}
+      --without-tutorial
+      --disable-wxwidgets
+      --without-qt
+      --without-x
+      --with-aquaterm
+    ]
+
+    system "./prepare" if build.head?
+    system "./configure", *args
+    ENV.deparallelize # or else emacs tries to edit the same file with two threads
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/gnuplot", "-e", <<~EOS
+      set terminal dumb;
+      set output "#{testpath}/graph.txt";
+      plot sin(x);
+    EOS
+    assert_predicate testpath/"graph.txt", :exist?
+  end
+end
